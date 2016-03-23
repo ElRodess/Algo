8e31e428ec6e674bddaf9b3239d7fcbc8e3348f8
IN 200 (Fond. info. 2) <in_200@user-VirtualBox.(none)>
2016-03-23 15:09:22 +0100

    coucou

--- test.c
+++ test.c
@@ -2,14 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct arete Arete;
+
+typedef struct sommet
+{
+        char nom;
+        Arete* suivants;
+}Sommet;
+
 typedef struct arete
 {
-	char nom[25];
-	char type[25];
-	char depart[25];
-	char arrivee[25];
+	char* nom;
+	int poids;
+	Sommet depart[25];
+	Sommet arrivee[25];
 }Arete;
 
+
+
+/*
 int nombre_de_ligne (FILE *fichier)
 {
 	int i = 0;
@@ -28,30 +43,31 @@ void affiche_arete(Arete a)
 	printf("depart: %s",a.depart);
 	printf("arrivee: %s\n",a.arrivee);
 }
-
+*/
 int main(int argc, char **argv)
 {
-	Arete A[5];
-	int i = 0;
-	int j = 0;
-	FILE *fichier  = fopen("test.txt","r");
-	int compteur = nombre_de_ligne(fichier);
-	rewind(fichier);
+	//~ Arete A[5];
+	//~ int i = 0;
+	//~ int j = 0;
+	//~ FILE *fichier  = fopen("test.txt","r");
+	//~ int compteur = nombre_de_ligne(fichier);
+	//~ rewind(fichier);
+	//~ 
+	//~ while(j != compteur)
+	//~ {
+		//~ fgets(A[i].nom,10,fichier);
+		//~ j ++;
+		//~ fgets(A[i].type,10,fichier);
+		//~ j ++;
+		//~ fgets(A[i].depart,10,fichier);
+		//~ j ++;
+		//~ fgets(A[i].arrivee,10,fichier);
+		//~ j ++;
+		//~ affiche_arete(A[i]);
+		//~ i++;
+	//~ }
+	//~ 
+	//~ fclose(fichier);
 	
-	while(j != compteur)
-	{
-		fgets(A[i].nom,10,fichier);
-		j ++;
-		fgets(A[i].type,10,fichier);
-		j ++;
-		fgets(A[i].depart,10,fichier);
-		j ++;
-		fgets(A[i].arrivee,10,fichier);
-		j ++;
-		affiche_arete(A[i]);
-		i++;
-	}
-	
-	fclose(fichier);
 	return 0;
 }
