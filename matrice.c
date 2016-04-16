64165194a187f813fc7443343b56cd62fdc00fcb
IN 200 (Fond. info. 2) <in_200@user-VirtualBox.(none)>
2016-04-16 15:39:17 +0200

    YOLO

--- matrice.c
+++ matrice.c
@@ -4,13 +4,13 @@
 
 typedef struct{
 	char nom[50];
-	int temps;
+	float temps;
 }Pistes;
 
 int main(int argc, char **argv)
 {
 	FILE *fichier  = fopen("Matrice.txt","r");
-	FILE *fichier1  = fopen("Pistes1.txt","r");
+	FILE *fichier1  = fopen("Pistes2.txt","r");
 	
 	char matrice[47][47][50];
 	int i,j;
@@ -25,20 +25,24 @@ int main(int argc, char **argv)
 	}
 	fclose(fichier);
 	
-	Pistes p[97];
+	Pistes p[99];
 	int n;
-	for (n = 0; n < 97; n ++)
+	for (n = 0; n < 99; n ++)
 	{
-		fscanf(fichier1,"%s %d", (p[n].nom),& (p[n].temps));
+		fscanf(fichier1,"%s %f", (p[n].nom), &(p[n].temps));
 	}
-	
+	//~ for (n = 0; n < 99; n ++)
+	//~ {
+		//~ printf("%s %f \n",p[n].nom, (p[n].temps));
+	//~ }
 	
 	//printf("%s\n",matrice[0][0]);
 	
 	fclose(fichier1);
 	
-	FILE *matricefichier = fopen("matrice_cout","w");
-	int cout[47][47];
+	FILE *matricefichier = fopen("matrice_cout2","w");
+	float cout[47][47];
+	int test = 0;
 	for (i = 0; i < 47; i++)
 	{
 		for (j = 0; j < 47; j ++)
@@ -46,14 +50,17 @@ int main(int argc, char **argv)
 			if (strcmp(matrice[i][j],"null") == 0) cout[i][j] = 0;
 			else 
 			{
-				for(n = 0;n < 97; n ++)
+				test =0;
+				for(n = 0;n < 99; n ++)
 				{
 					if(strcmp(matrice[i][j],(p[n].nom)) == 0) 
 					//if (matrice[i][j] == p[n].nom)
 					{
 						cout[i][j] = p[n].temps;
+						test = 1;
 					}
 				}
+				if (test == 0)cout[i][j] = -1;
 			}
 		}
 	}
@@ -62,7 +69,7 @@ int main(int argc, char **argv)
 	{
 		for (j = 0; j < 47; j ++)
 		{
-			fprintf(matricefichier,"%d ",cout[i][j]);
+			fprintf(matricefichier,"%f ",cout[i][j]);
 		}
 		fprintf(matricefichier,"\n");
 		
