24e889cd1dc8b62427698020735a566fa0d02ebc
Utilisateur <ronan.damonville@gmail.com>
2016-04-19 15:35:45 +0200

    algo de dijtra avec tous les noms des pistes, y compris en entre et arrive

--- dijtra.c
+++ dijtra.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <string.h> 
 #define INFINITY 9999.00
  
-void dijkstra(float G[47][47], int n, int depart, int arrivee);
+void dijkstra(float G[47][47], int n, int depart, int arrivee, char sommets[47][50]);
+void rentre_sommets (char sommets[47][50]);
+void affiche_sommets();
+int choix_sommets(char sommets[47][50]);
 
 int main(){
-	int u, v;
+	int depart, arrivee;
 	int choix = 0;
 	int boolean = -1;
 	FILE *fichier;
+	char sommets[47][50];
 	
 	while (boolean != 0)
 	{
@@ -19,6 +24,8 @@ int main(){
 	
 	if (choix == 1) fichier  = fopen("matrice_cout1","r");
 	else fichier = fopen("matrice_cout2","r");
+	
+	rentre_sommets(sommets);
 	
 	float G[47][47];
 	int i,j;
@@ -32,16 +39,27 @@ int main(){
 		}
 	}
 	fclose(fichier);
+	affiche_sommets();
 	printf("Entrer le sommet de depart: \n");
-	scanf("%d", &u);
+	depart = choix_sommets(sommets);
+	if (depart == -1)
+	{
+		printf("Le sommet rentre comme depart est incorrect\n");
+		return -1;
+	}
 	printf("Entrer le sommet d'arrivee: \n");
-	scanf("%d", &v);
-	dijkstra(G,47,u,v);
+	arrivee = choix_sommets(sommets);
+	if (arrivee == -1)
+	{
+		printf("Le sommet rentre comme arrive est incorect\n");
+		return -1;
+	}
+	dijkstra(G,47,depart,arrivee,sommets);
 	
 	return 0;
 }
  
-void dijkstra(float G[47][47], int n, int depart, int arrivee)
+void dijkstra(float G[47][47], int n, int depart, int arrivee, char sommets[47][50])
 {
 	float cout[47][47], distance[47], pred[47];
 	float visite[47], compteur, mindistance ; 
@@ -94,15 +112,74 @@ void dijkstra(float G[47][47], int n, int depart, int arrivee)
 	 **/
 		if(i!=depart)
 		{
-			printf("\nLa distance entre %d et %d est %f", depart, arrivee, distance[i]);
-			printf("\nLe trajet est = %d", i);
+			printf("\nLa distance entre %s et %s est %f", sommets[depart], sommets[arrivee], distance[i]);
+			printf("\nLe trajet est = %s", sommets[i]);
 			j=i;
 			do
 			{
 				j=pred[j];
-				printf(" <- %d", j);
+				printf(" <- %s", sommets[j]);
 			}
 			while(j!=depart);
 		}
 		printf("\n");
+}
+
+void rentre_sommets(char sommets[47][50])
+{
+	FILE *fichier = fopen("Sommets.txt","r");
+	int i;
+	for (i = 0; i < 47; i ++)
+	{
+		fscanf(fichier, "%s", sommets[i]);
+	}
+	
+	fclose(fichier);
+}
+
+void affiche_sommets()
+{
+	printf("Vous pouvez partir des sommets suivants:\n");
+	printf("La-Villette\n");
+	printf("MontFrais\n");
+	printf("Vaujany\n");
+	printf("Alpette\n");
+	printf("Dome-Des-Petites-Rousses\n");
+	printf("Pic-Blanc\n");
+	printf("Glacier-De-Sarenne\n");
+	printf("Clocher-De-Macle\n");
+	printf("Plat-Des-Marmottes\n");
+	printf("Alpe-D-Huez\n");
+	printf("Oz-En-Oisans\n");
+	printf("Signal\n");
+	printf("Villard-Reculas\n");
+	printf("Huez\n");
+	printf("Signal-De-L-Homme\n");
+	printf("Auris-En-Oisans\n");
+	printf("Le-Chatelard\n");
+	printf("Enversin\n");
+}
+
+int choix_sommets(char sommets[47][50])
+{
+	char chaine[50];
+	scanf("%s",chaine);
+	if (strcmp(chaine,sommets[0]) == 0) return 0;
+	else if (strcmp(chaine,sommets[1]) == 0) return 1;
+	else if (strcmp(chaine,sommets[3])== 0) return 3;
+	else if (strcmp(chaine,sommets[4]) == 0) return 4;
+	else if (strcmp(chaine,sommets[8]) == 0) return 8;
+	else if (strcmp(chaine,sommets[13]) == 0) return 13;
+	else if (strcmp(chaine,sommets[14]) == 0) return 14;
+	else if (strcmp(chaine,sommets[17]) == 0) return 17;
+	else if (strcmp(chaine,sommets[19]) == 0) return 19;
+	else if (strcmp(chaine,sommets[20]) == 0) return 20;
+	else if (strcmp(chaine,sommets[23]) == 0) return 23;
+	else if (strcmp(chaine,sommets[28]) == 0) return 28;
+	else if (strcmp(chaine,sommets[29]) == 0) return 29;
+	else if (strcmp(chaine,sommets[36]) == 0) return 36;
+	else if (strcmp(chaine,sommets[38]) == 0) return 38;
+	else if (strcmp(chaine,sommets[43]) == 0) return 43;
+	else if (strcmp(chaine,sommets[44]) == 0) return 44;
+	else return -1;
 }
